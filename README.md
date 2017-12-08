0cc3a6c960bacabf108955a274d8eb07c2b3f458
b0tting <otster@hotmail.com>
2017-12-08 17:23:35 +0100

    Inital commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 To add additional fields, first build the WebLogic full client jar from an existing WebLogic installation. See https://docs.oracle.com/cd/E24329_01/web.1211/e24378/jarbuilder.htm#SACLT421 - this will turn out at about 100mb.
 
-
+(...working on it!)
--- src/nl/qualogy/wls/HTTPAccessFields.java
+++ src/nl/qualogy/wls/HTTPAccessFields.java
@@ -4,7 +4,6 @@ import weblogic.servlet.logging.CustomELFLogger;
 import weblogic.servlet.logging.FormatStringBuffer;
 import weblogic.servlet.logging.HttpAccountingInfo;
 
-
 public class HTTPAccessFields implements CustomELFLogger {
     public void logField(HttpAccountingInfo metrics, FormatStringBuffer buff) {
         buff.appendValueOrDash(metrics.getRemoteUser());
